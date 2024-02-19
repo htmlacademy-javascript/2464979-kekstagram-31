2089bac0b55b9b1ba67eed855f5236348f474db4
Вика <ledivik@mail.ru>
2024-02-20 00:40:24 +0300

    Попытка №6

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
 const stringLength = function (string, maxLength) {
-   return string.length <= maxLength;
+  return string.length <= maxLength;
 };
 
 stringLength('оченьважныйтекст', 10);
 
 const makePolindromString = function (string) {
-    const normalString = string.replaceAll(' ','').toLowerCase();
-    let reverseString = '';
-    for (let i = normalString.length - 1; i >= 0; i--) {
-        reverseString += normalString[i];
-    };
-    return reverseString === normalString;
+  const normalString = string.replaceAll(' ','').toLowerCase();
+  let reverseString = '';
+  for (let i = normalString.length - 1; i >= 0; i--) {
+    reverseString += normalString[i];
+  }
+  return reverseString === normalString;
 };
 
 makePolindromString('топот');

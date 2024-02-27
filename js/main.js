8752f552fe6a4cddc3597e2f96fbe07d0086d683
Вика <ledivik@mail.ru>
2024-02-27 19:37:17 +0300

    Больше деталей (доработка кода)

--- js/main.js
+++ js/main.js
@@ -7,34 +7,36 @@ const COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.'
 ];
 
-const getRandomInteger = (a,b) => {
+function getRandomInteger(a,b) {
   const lower = Math.ceil(Math.min(a,b));
   const upper = Math.floor(Math.max(a,b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+}
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createCount = () => {
+function createCount() {
   let count = 0;
-  return function () {
+  return function() {
     count += 1;
     return count;
   };
-};
+}
 
-const idComment = createCount ();
-const idPicture = createCount ();
+const idComment = createCount();
+const idPicture = createCount();
 
-const createObject = () => ({
-  id: idComment (),
-  avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
-  message: getRandomArrayElement(COMMENTS),
-  name: getRandomArrayElement(NAMES),
-});
+function createObject() {
+  return {
+    id: idComment(),
+    avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+    message: getRandomArrayElement(COMMENTS),
+    name: getRandomArrayElement(NAMES),
+  };
+}
 
-const createPhotoDescription = function () {
+const createPhotoDescription = function() {
   return {
     id: idPicture(),
     url: `photos/${getRandomInteger(1,25)}.jpg`,
@@ -44,4 +46,6 @@ const createPhotoDescription = function () {
   };
 };
 
-createPhotoDescription();
+const getArrayPhotos = Array.from({length: 25}, createPhotoDescription);
+
+window.console.log(getArrayPhotos);

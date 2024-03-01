e7a717b25d1659bf6d6f02c982b1ddd53767f11e
Вика <ledivik@mail.ru>
2024-03-01 23:06:02 +0300

    Module5-task2

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,35 @@ const makePolindromString = function (string) {
 makePolindromString('топот');
 
 // Задание 3
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+
+function getMinutes(time) {
+  const [hours, minutes] = time.split(':');
+  return Number(hours) * 60 + Number(minutes);
+}
+
+function calcMeetingTime(startTime, endTime, meetingTime, meetingDuration) {
+  const minutesStartTime = getMinutes(startTime);
+  const minutesEndTime = getMinutes(endTime);
+  const minutesMeetingTime = getMinutes(meetingTime);
+  if (minutesMeetingTime < minutesStartTime) {
+    return false;
+  } if (minutesMeetingTime >= minutesEndTime) {
+    return false;
+  } if (minutesEndTime - minutesMeetingTime >= meetingDuration) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+calcMeetingTime('8:00', '17:30', '14:00', 90);// true
+calcMeetingTime('8:0', '10:0', '8:0', 120);// true
+calcMeetingTime('08:00', '14:30', '14:00', 90); // false
+calcMeetingTime('14:00', '17:30', '08:0', 90);// false
+calcMeetingTime('8:00', '17:30', '08:00', 900); // false

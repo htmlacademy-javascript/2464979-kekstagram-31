8142c6921c0b574e5f49b77c90f40838da7ddb76
Вика <ledivik@mail.ru>
2024-03-06 23:30:48 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 import { getArrayPhotos } from './create-array-miniatures.js';
 window.console.log(getArrayPhotos);
+
+import { getThumbnails } from './create-thumbnail.js';
+window.console.log(getThumbnails);

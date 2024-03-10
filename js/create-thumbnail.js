9b40c933667babfbb79eed24081beae3464a3715
Вика <ledivik@mail.ru>
2024-03-10 22:23:44 +0300

    Все работает!

--- js/create-thumbnail.js
+++ js/create-thumbnail.js
@@ -1,8 +1,3 @@
-/* Адрес изображения url подставьте как атрибут src изображения.
-Описание изображения description подставьте в атрибут alt изображения.
-Количество лайков likes выведите в блок .picture__likes.
-Количество комментариев comments выведите в блок .picture__comments */
-
 const picturesContainer = document.querySelector('.pictures');
 
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
@@ -11,20 +6,19 @@ function createPhoto (photoData) {
   const photo = templatePicture.cloneNode(true);
   photo.querySelector('.picture__img').src = photoData.url;
   photo.querySelector('.picture__img').alt = photoData.description;
-  photo.querySelector('.picture__comments').textContent = photoData.comments;
+  photo.querySelector('.picture__comments').textContent = photoData.comments.length;
   photo.querySelector('.picture__likes').textContent = photoData.likes;
 
   return photo;
 }
 
-function getThumbnails (data) {
+/* function getThumbnails (data) {
   const createListFragment = document.createDocumentFragment();
-
   data.forEach(({ url, description, likes, comments }) => {
     createListFragment.append(createPhoto({ url, description, likes, comments }));
   });
+  picturesContainer.append(createListFragment); */
 
-  picturesContainer.append(createListFragment);
-}
+const getThumbnails = (data) => picturesContainer.append(...data.map(createPhoto));
 
 export { getThumbnails };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 import { getArrayPhotos } from './create-array-miniatures.js';
-window.console.log(getArrayPhotos);
-
 import { getThumbnails } from './create-thumbnail.js';
-window.console.log(getThumbnails);
-
-/* const picture = getArrayPhotos();
-getThumbnails(picture); */
 
 getThumbnails(getArrayPhotos);
